69ba29f70a4d39ac6d3325b03d7f3fb803beccc5
PavelShturm85 <pavel.shturm@gmail.com>
2017-11-22 00:58:00 +0400

    Проверка сложности пароля.

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+**This module insert on terminal the password strength.**
+
+# Quickstart
+**Ways to use:**
+- Have to use  module `password_strenght.py` after `python3` with argument `'password'` in terminal.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python3 password_strenght.py 'password'
+
+Password strength is -1- from 10.
+
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,29 @@
 
+import re
+import sys
+
 
 def get_password_strength(password):
-    pass
+    password_strength = 0
+    if re.search('[A-Z]', password):
+        password_strength += 1
+    if re.search('[a-z]', password):
+        password_strength += 1
+    if re.search('[0-9]', password):
+        password_strength += 1
+    if re.search('[$#@]', password):
+        password_strength += 1
+    if re.search('\s', password):
+        password_strength += 1
+    if len(set(password)) >= len(password)*0.8 and len(password) >= 10:
+        password_strength += 5
+    return password_strength
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        input_password = sys.argv[1]
+    else:
+        input_password = input('Введите пароль: ')
+    print("Password strength is -{}- from 10.".format(
+        get_password_strength(input_password)))
